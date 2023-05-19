7f92e0bb512a866a98dc8346c954ce55e870b20e
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-05-19 16:57:32 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit of base 10
+ * Return: 0 Always
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i != 58)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
