21b665c9822482d269640aa0b847c3fb519c6ac9
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-05-18 18:06:46 -0700

    last one

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - a program that print a quote
+ * Return: 1
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
