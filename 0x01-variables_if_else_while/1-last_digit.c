183adef41c800d81f78860b61e112af5ab4f1a49
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-05-19 18:35:26 -0700

    update task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0 && n % 10 != 0)
+	if (n > 5 && n % 10 != 0)
 		printf("last digit %d is %d and is greater than 5\n", n, n % 10);
-	else if (n < 0 && n % 10 != 0)
+	else if (n < 6 && n % 10 != 0)
 		printf("last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-	else
+	else if (n == 0)
 		printf("last digit of %d is 0\n", n);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combination of two digits
+ * Return: 0 Always
+ */
+int main(void)
+{
+
+
+}
