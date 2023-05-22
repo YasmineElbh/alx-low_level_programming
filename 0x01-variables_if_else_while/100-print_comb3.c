2c66c36cb70e2d88907bfd021fc63472567862b1
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-05-22 12:13:32 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,6 +6,21 @@
  */
 int main(void)
 {
+	int i = 0;
+	int j;
 
-
+	for (; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
 }
