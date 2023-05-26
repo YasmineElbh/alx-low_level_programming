05049cfabca3beb926b86d7bec1893abdf5e66f1
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-05-26 15:56:23 -0700

    update task 10

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,12 +10,10 @@ int main(void)
 	unsigned long int i, n;
 
 	n = 612852475143;
-	for (i = 2; i <= n; i++)
+	for (i = 3; i * i < n; i = i + 2)
 	{
-		if (i % n == 0)
+		if ((i % n == 0) && (n != i))
 			n = n / i;
-		else
-			i++;
 	}
 	printf("%lu\n", n);
 	return (0);
