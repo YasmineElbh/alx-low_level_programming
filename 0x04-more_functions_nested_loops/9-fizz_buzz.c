41da5cdbf60a24d90f0daeae9bade4738ae48007
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-05-26 16:03:28 -0700

    update fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,14 @@ int main(void)
 
 	for (i = 1;  i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3) == 0)
 			printf("Fizz ");
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 			printf("Buzz ");
-		else if (i % 15 == 0)
+		else if ((i % 15) == 0)
 			printf("FizzBuzz ");
 		else
 			printf("%i ", i);
-		if (i < 100)
-			printf(" ");
 	}
 	printf("\n");
 	return (0);
