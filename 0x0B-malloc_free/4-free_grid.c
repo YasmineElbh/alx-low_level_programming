934d3770115d4248c30596cc18d1455e3e228133
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-06-13 12:39:28 -0700

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * free_grid - function tha free 2 dimansional grid
+ * @grid: grid
+ * @height: height
+ * Return: result
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
