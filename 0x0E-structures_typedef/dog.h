49d7b4fafb3e8c375413a4948d7f969a41af64f5
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-06-20 16:30:42 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef Structur
+#define Structur
+
+/**
+ * Structur - define dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
