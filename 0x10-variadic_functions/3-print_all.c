20f6f8ec9f1588034b4d84c077267aa84dcb932e
YasmineElbh <elbahiyasmine.4@gmail.com>
2023-06-22 17:49:28 -0700

    last

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_all -  a function that prints anything
+ * @format: format
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *sep;
+	char op;
+	float fii;
+	va_list arr;
+
+	va_start(arr, format);
+	while (format[i])
+	{
+		if (format[i] == 'o')
+		{
+			op = va_arg(arr, int);
+			printf("%o", op);
+		}
+		else if (format[i] == 'f')
+		{
+			fii = va_arg(arr, double);
+			printf("%f", fii);
+		}
+		else if (format[i] == 's')
+		{
+			sep = va_arg(arr, char*);
+			if (sep == NULL)
+				sep = "(nil)";
+			else
+				printf("%s", sep);
+		}
+		i++;
+	}
+	va_end(arr);
+	printf("\n");
+}
